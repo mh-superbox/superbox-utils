97c1ef271481b9e63f901fbe144bb67f458c6925
Michael Hacker <mh@superbox.one>
2022-10-21 08:46:09 +0200

    Updated workflow

--- tests/unittests/yaml/test_loader.py
+++ tests/unittests/yaml/test_loader.py
@@ -2,23 +2,23 @@ from pathlib import Path
 from typing import Union
 
 import pytest
+from unittests.yaml.test_loader_data import INVALID_YAML
+from unittests.yaml.test_loader_data import VALID_YAML
 
 from superbox_utils.config.exception import ConfigException
 from superbox_utils.yaml.loader import yaml_loader_safe
-from unittests.yaml.test_loader_data import INVALID_YAML
-from unittests.yaml.test_loader_data import VALID_YAML
 
 
 class TestHappyPathLoader:
     @pytest.mark.parametrize(
-        "content_to_file, expected",
+        "_content_to_file, expected",
         [
             (("test.yaml", VALID_YAML), dict),
         ],
-        indirect=["content_to_file"],
+        indirect=["_content_to_file"],
     )
-    def test_yaml_loader_safe(self, content_to_file: Path, expected: type):
-        yaml_data: Union[dict, list] = yaml_loader_safe(content_to_file)
+    def test_yaml_loader_safe(self, _content_to_file: Path, expected: type):
+        yaml_data: Union[dict, list] = yaml_loader_safe(_content_to_file)
         print(yaml_data)
 
         assert isinstance(yaml_data, expected)
@@ -26,17 +26,17 @@ class TestHappyPathLoader:
 
 class TestUnHappyPathLoader:
     @pytest.mark.parametrize(
-        "content_to_file, expected",
+        "_content_to_file, expected",
         [
             (
                 ("test.yaml", INVALID_YAML),
                 """Can't read YAML file!\n  in "<unicode string>", line 3, column 1:\n    \n    ^""",
             ),
         ],
-        indirect=["content_to_file"],
+        indirect=["_content_to_file"],
     )
-    def test_yaml_loader_safe(self, content_to_file: Path, expected: str):
+    def test_yaml_loader_safe(self, _content_to_file: Path, expected: str):
         with pytest.raises(ConfigException) as error:
-            yaml_loader_safe(content_to_file)
+            yaml_loader_safe(_content_to_file)
 
         assert str(error.value) == expected
